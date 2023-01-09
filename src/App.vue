976a57cb7c19b7842318aa486c49ec3c5aeefd24
changfeng <changfeng@gaoding.com>
2023-01-09 17:26:41 +0800

    reproduce async component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <script lang="ts" setup>
-import HotTable from "@handsontable/vue";
+import { defineAsyncComponent } from "vue";
 
-import "handsontable/dist/handsontable.full.css";
-
-const settings = {
-  data: [
-    ["", "Ford", "Volvo", "Toyota", "Honda"],
-    ["2016", 10, 11, 12, 13],
-    ["2017", 20, 11, 14, 13],
-    ["2018", 30, 15, 12, 13]
-  ],
-  colHeaders: true,
-  rowHeaders: true
-};
+const HelloWorld = defineAsyncComponent(() =>
+  import("./components/HelloWorld.vue")
+);
 </script>
 
 <template>
   <div id="hot-preview">
-    <HotTable
-      :settings="settings"
-      license-key="non-commercial-and-evaluation"
-    />
+    <HelloWorld />
   </div>
 </template>
 
